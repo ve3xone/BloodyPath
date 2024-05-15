eeddd083cb5c602dc7bae09f57b9a661a7c55b35
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-05-16 02:33:23 +0500

    Отделил draw от player + HpBar тоже был отделен

---
+++ Player/BasePlayerDrawer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BloodyPath.Player
+{
+    public class BasePlayerDrawer
+    {
+        private BasePlayer player;
+        private SpriteBatch spriteBatch;
+
+        public BasePlayerDrawer(SpriteBatch spriteBatch, BasePlayer player)
+        {
+            this.player = player;
+            this.spriteBatch = spriteBatch;
+        }
+
+        public void Draw()
+        {
+            // Draw player
+            if (player.IsAttacking)
+            {
+                spriteBatch.Draw(player.DamagedTexture, player.Position, Color.White);
+            }
+            else
+            {
+                spriteBatch.Draw(player.Texture, player.Position, Color.White);
+            }
+        }
+
+        public void DrawHpPlayerBar(Vector2 Pos)
+        {
+            // Draw HP bar background
+            spriteBatch.Draw(player.HpTexture, new Rectangle((int)Pos.X, (int)Pos.Y, 100, 10), Color.Gray);
+
+            // Draw filled portion of HP bar
+            int fillWidth = (int)(player.HP / (float)100 * 100);
+            spriteBatch.Draw(player.HpTexture, new Rectangle((int)Pos.X, (int)Pos.Y, fillWidth, 10), Color.Red);
+        }
+    }
+}
