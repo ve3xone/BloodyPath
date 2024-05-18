e09fa1ff02ee26375f2566779672c363792c1e98
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-05-18 16:16:02 +0500

    Сделал меньше урон атаки
    Я думаю лучшее для геймплея, думаю ещё добавить урон атаки для ног и отдельнно для рук

--- Models/BasePlayer.cs
+++ Models/BasePlayer.cs
@@ -9,7 +9,7 @@ public class BasePlayer
     public Vector2 Position = Vector2.Zero;
     public int HP = 100;
     public readonly int PlayerSpeed = 3;
-    public readonly int AttackDamage = 10;
+    public readonly int AttackDamage = 6;
 
     public BasePlayer(Vector2 position)
     {
