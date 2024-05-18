c8858d5d28eb8931439ce943f48f0481e6a459a6
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-05-19 02:50:31 +0500

    Всё объяденил в один класс Button

---
+++ Models/Button.cs
@@ -0,0 +1,12 @@
+﻿using BloodyPath.Controllers;
+using BloodyPath.Views;
+
+namespace BloodyPath.Models
+{
+    public class Button
+    {
+        public ClickableText ClickableText;
+        public ClickableTextDrawer ClickableTextDrawer;
+        public ClickableTextController ClickableTextController;
+    }
+}
