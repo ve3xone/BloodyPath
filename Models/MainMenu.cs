72e362e51b8dfea103b1e77de735bb564ee118ca
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-05-19 02:53:55 +0500

    Объяденил все что связнно с главным меню в общ класс

---
+++ Models/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using BloodyPath.Controllers;
+using BloodyPath.Views;
+
+namespace BloodyPath.Models
+{
+    public class MainMenu
+    {
+        public MainMenuScreen MenuScreen;
+        public MainMenuScreenDrawer MenuScreenDrawer;
+        public MainMenuScreenController MenuScreenController;
+    }
+}
