1b6ce8cc64b77df442d2abcd2ec681d198226842
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-05-18 07:02:02 +0500

    Решил вынести всю структуру Player в Persona

---
+++ Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿using BloodyPath.Controller;
+using BloodyPath.View;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace BloodyPath.Models;
+
+public class Persona
+{
+    public BasePlayer Player;
+    public BasePlayerDrawer PlayerDrawer;
+    public Dictionary<string, Keys> PlayerKeyMappings;
+    public BasePlayerController PlayerController;
+}
