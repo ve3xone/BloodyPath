d89a50de6da5e6810811201c7fa64626b2b6e2b5
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-05-19 14:35:23 +0500

    Рефакторинг

--- Controllers/MainMenuScreenController.cs
+++ Controllers/MainMenuScreenController.cs
@@ -1,21 +1,19 @@
 ﻿using BloodyPath.Models;
-using Microsoft.Xna.Framework;
 
-namespace BloodyPath.Controllers
+namespace BloodyPath.Controllers;
+
+public class MainMenuScreenController
 {
-    public class MainMenuScreenController
-    {
-        private MainMenuScreen MainMenuScreen { get; set; }
-        public MainMenuScreenController(MainMenuScreen mainMenuScreen) 
-        { 
-            this.MainMenuScreen = mainMenuScreen;
-        }
+    private MainMenuScreen MainMenuScreen { get; set; }
+    public MainMenuScreenController(MainMenuScreen mainMenuScreen) 
+    { 
+        this.MainMenuScreen = mainMenuScreen;
+    }
 
-        public void Update()
-        {
-            MainMenuScreen.PlayButton.ClickableTextController.Update();
-            MainMenuScreen.SettingsButton.ClickableTextController.Update();
-            MainMenuScreen.ExitButton.ClickableTextController.Update();
-        }
+    public void Update()
+    {
+        MainMenuScreen.PlayButton.ClickableTextController.Update();
+        MainMenuScreen.SettingsButton.ClickableTextController.Update();
+        MainMenuScreen.ExitButton.ClickableTextController.Update();
     }
 }
