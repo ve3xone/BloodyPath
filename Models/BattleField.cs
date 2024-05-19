53b9cc876b7bf7f93480950a44e21e33ab96c55b
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-05-19 11:34:48 +0500

    Поле битвы

---
+++ Models/BattleField.cs
@@ -0,0 +1,11 @@
+﻿using BloodyPath.Controllers;
+using BloodyPath.Views;
+
+namespace BloodyPath.Models;
+
+public class BattleField
+{
+    public BattleFieldScreen Screen;
+    public BattleFieldScreenDrawer ScreenDrawer;
+    public BattleFieldScreenController ScreenController;
+}
