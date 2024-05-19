a544910207e7fd4ab12063c55b07b860ba9c0195
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-05-19 11:29:15 +0500

    Рефакторинг PlayerDrawer

--- Views/BasePlayerDrawer.cs
+++ Views/BasePlayerDrawer.cs
@@ -6,53 +6,50 @@ namespace BloodyPath.View;
 
 public class BasePlayerDrawer
 {
-    private readonly SpriteBatch SpriteBatch;
     private readonly BasePlayer Player;
     public Texture2D PlayerTexture { get; private set; }
     public Texture2D PlayerDamagedTexture { get; private set; }
     public Texture2D PlayerHpTexture { get; private set; }
 
-    public BasePlayerDrawer(SpriteBatch spriteBatch,
-                                         BasePlayer player,
+    public BasePlayerDrawer(BasePlayer player,
                                          Texture2D playerTexture,
                                          Texture2D playerDamagedTexture,
                                          Texture2D playerHpTexture)
     {
         Player = player;
-        SpriteBatch = spriteBatch;
         PlayerTexture = playerTexture;
         PlayerDamagedTexture = playerDamagedTexture;
         PlayerHpTexture = playerHpTexture;
     }
 
-    public void Draw()
+    public void Draw(SpriteBatch spriteBatch)
     {
         if (Player.IsAttacking)
         {
-            SpriteBatch.Draw(PlayerDamagedTexture, Player.Position, Color.White);
+            spriteBatch.Draw(PlayerDamagedTexture, Player.Position, Color.White);
         }
         else
         {
-            SpriteBatch.Draw(PlayerTexture, Player.Position, Color.White);
+            spriteBatch.Draw(PlayerTexture, Player.Position, Color.White);
         }
     }
 
-    public void DrawHpPlayerBar(Vector2 Pos, bool Reverse, SpriteFont font)
+    public void DrawHpPlayerBar(SpriteBatch spriteBatch, SpriteFont font, Vector2 Pos, bool Reverse)
     {
         // Draw HP bar background
-        SpriteBatch.Draw(PlayerHpTexture, new Rectangle((int)Pos.X, (int)Pos.Y, 300, 15), Color.Gray);
+        spriteBatch.Draw(PlayerHpTexture, new Rectangle((int)Pos.X, (int)Pos.Y, 300, 25), Color.Gray);
 
         // Draw filled portion of HP bar
         int maxWidth = 300;
         int fillWidth = (int)(Player.HP / (float)100 * maxWidth);
 
         // Позиция текста "Player 1"
-        Vector2 playerTextPos = new(Pos.X, Pos.Y - 28);
+        Vector2 playerTextPos = new(Pos.X, Pos.Y - 26);
 
         if (Reverse)
         {
-            SpriteBatch.DrawString(font, "Player 1", playerTextPos, Color.White);
-            SpriteBatch.Draw(PlayerHpTexture, 
+            spriteBatch.DrawString(font, "Player 1", playerTextPos, Color.White);
+            spriteBatch.Draw(PlayerHpTexture, 
                                       new Rectangle((int)Pos.X, (int)Pos.Y, fillWidth, 25), 
                                       Color.Red);
         }
@@ -64,10 +61,10 @@ public class BasePlayerDrawer
             Vector2 playerTextSize = font.MeasureString("Player 2");
 
             // Позиция текста "Player 2"
-            playerTextPos = new Vector2(Pos.X + maxWidth - playerTextSize.X + 3, Pos.Y - 28);
+            playerTextPos = new Vector2(Pos.X + maxWidth - playerTextSize.X + 3, Pos.Y - 26);
 
-            SpriteBatch.DrawString(font, "Player 2", playerTextPos, Color.White);
-            SpriteBatch.Draw(PlayerHpTexture, 
+            spriteBatch.DrawString(font, "Player 2", playerTextPos, Color.White);
+            spriteBatch.Draw(PlayerHpTexture, 
                                       new Rectangle((int)Pos.X + fillWidth, (int)Pos.Y, maxWidth - fillWidth, 25), 
                                       Color.Red);
         }
