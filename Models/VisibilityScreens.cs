329d7d0b02b7b05e8cf5ab0964adc26e60c4f30c
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-05-19 14:32:20 +0500

    Сделал отдельный класс для отображения экранов

---
+++ Models/VisibilityScreens.cs
@@ -0,0 +1,13 @@
+﻿namespace BloodyPath.Models;
+
+public class VisibilityScreens
+{
+    public bool MainMenuIsVisible;
+    public bool BattleFieldIsVisible;
+
+    public VisibilityScreens()
+    {
+        this.MainMenuIsVisible = true;
+        this.BattleFieldIsVisible = false;
+    }
+}
