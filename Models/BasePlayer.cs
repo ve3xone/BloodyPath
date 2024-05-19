8e25d5cb4f4827eebd78e33b9ef85a6f98d7b241
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-05-19 20:42:05 +0500

    Добавил будующие типы для будующих атак

--- Models/BasePlayer.cs
+++ Models/BasePlayer.cs
@@ -4,7 +4,12 @@ namespace BloodyPath.Models;
 
 public class BasePlayer
 {
+    //Сделать чтоб была атака рукой и ногами
     public bool IsAttacking { get; set; }
+    public bool IsAttackingHands { get; set; }
+    public bool IsAttackingFeet { get; set; }
+    //Также сделать когда он мертный
+    public bool IsKilled { get; set; }
 
     public Vector2 Position = Vector2.Zero;
     public int HP = 100;
