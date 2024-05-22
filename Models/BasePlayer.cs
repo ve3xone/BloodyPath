17f930a3d12c8136856149ff0c5c05ebb8e7ed3e
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-05-22 10:10:55 +0500

    Теперь player умеет присидать (отклоняться от кулоков)
    Также теперь есть у каждого player победы и номер + в каком направлении он находится

--- Models/BasePlayer.cs
+++ Models/BasePlayer.cs
@@ -4,21 +4,27 @@ namespace BloodyPath.Models;
 
 public class BasePlayer
 {
-    //Сделать чтоб была атака рукой и ногами
     public bool IsAttacking { get; set; }
     public bool IsAttackingHands { get; set; }
     public bool IsAttackingFeet { get; set; }
-    //Также сделать когда он мертный
+    public bool IsDucked { get; set; }
     public bool IsKilled { get; set; }
+    public bool IsLeftTexture { get; set; }
 
+    public int Number { get; set; }
     public Vector2 Position = Vector2.Zero;
     public int HP = 100;
+    public int Victories = 0;
     public readonly int PlayerSpeed = 3;
     public readonly int AttackDamage = 6;
 
-    public BasePlayer(Vector2 position)
+    public BasePlayer(Vector2 position, bool isLeftTexture, int number)
     {
         Position = position;
-        IsAttacking = true;
+        IsAttackingHands = true;
+        IsAttackingFeet = true;
+        IsLeftTexture = isLeftTexture;
+        IsDucked = false;
+        Number = number;
     }
 }
