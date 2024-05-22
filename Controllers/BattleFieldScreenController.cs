4719bf03fd7b54f76850c2cc83b1354fd10bfc87
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-05-22 22:23:17 +0500

    Добавил gameTime в battleFieldContoller
    А также был пофикшен баг когда выходил в главное меню а на battlefield шло время

--- Controllers/BattleFieldScreenController.cs
+++ Controllers/BattleFieldScreenController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework.Input;
 using BloodyPath.Models;
+using Microsoft.Xna.Framework;
 
 namespace BloodyPath.Controllers;
 
@@ -12,24 +13,27 @@ public class BattleFieldScreenController
         BattleFieldScreen = battleFieldScreen;
     }
 
-    public void Update(KeyboardState keyboardState)
+    public void Update(GameTime gameTime, KeyboardState keyboardState)
     {
-        BattleFieldScreen.Persona1.PlayerController.Update(keyboardState, 
-                                                           BattleFieldScreen.Persona2.Player, 
-                                                           BattleFieldScreen.GroundRectangle, 
-                                                           BattleFieldScreen.Game.GraphicsDevice);
-
-        BattleFieldScreen.Persona2.PlayerController.Update(keyboardState, 
-                                                           BattleFieldScreen.Persona1.Player, 
-                                                           BattleFieldScreen.GroundRectangle, 
-                                                           BattleFieldScreen.Game.GraphicsDevice);
-
-        if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+        if (BattleFieldScreen.VisibilityScreens.BattleFieldIsVisible)
         {
-            BattleFieldScreen.VisibilityScreens.MainMenuIsVisible = true;
-            BattleFieldScreen.ResetScreen();
+            BattleFieldScreen.Persona1.PlayerController.Update(gameTime, keyboardState,
+                                                               BattleFieldScreen.Persona2.Player,
+                                                               BattleFieldScreen.GroundRectangle,
+                                                               BattleFieldScreen.Game.GraphicsDevice);
 
-            BattleFieldScreen.VisibilityScreens.BattleFieldIsVisible = false;
+            BattleFieldScreen.Persona2.PlayerController.Update(gameTime, keyboardState,
+                                                               BattleFieldScreen.Persona1.Player,
+                                                               BattleFieldScreen.GroundRectangle,
+                                                               BattleFieldScreen.Game.GraphicsDevice);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                BattleFieldScreen.VisibilityScreens.MainMenuIsVisible = true;
+                BattleFieldScreen.ResetScreen();
+
+                BattleFieldScreen.VisibilityScreens.BattleFieldIsVisible = false;
+            }
         }
     }
 }
