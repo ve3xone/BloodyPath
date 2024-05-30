043fdc2570ad473034552f5cf4d19b78abe577b8
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-05-30 16:46:46 +0500

    MVC по регулятору громкости

--- Models/SliderVolume.cs
+++ Models/SliderVolume.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Media;
 
 namespace BloodyPath.Models;
@@ -8,44 +7,13 @@ public class SliderVolume
 {
     public float Volume { get; set; } = MediaPlayer.Volume;
     public bool IsMuted { get; set; } = MediaPlayer.IsMuted;
-    public Texture2D SliderBarTexture;
-    public Texture2D SliderButtonTexture;
-    public Texture2D MuteTexture;
-    public Texture2D UnmuteTexture;
     public Vector2 SliderPosition;
     public Rectangle SliderBarRect;
     public Rectangle SliderButtonRect;
     public Rectangle MuteButtonRect;
 
-    public SliderVolume(float volume,
-                        Texture2D sliderBarTexture, 
-                        Texture2D sliderButtonTexture, 
-                        Texture2D muteTexture, 
-                        Texture2D unmuteTexture)
+    public SliderVolume(float volume)
     {
         Volume = volume;
-        this.SliderBarTexture = sliderBarTexture;
-        this.SliderButtonTexture = sliderButtonTexture;
-        this.MuteTexture = muteTexture;
-        this.UnmuteTexture = unmuteTexture;
-    }
-
-    public void LoadContent()
-    {
-        // Установка начальной позиции
-        SliderPosition = new Vector2(100, 488);
-
-        // Определение размеров и положения ползунка
-        SliderBarRect = new Rectangle((int)SliderPosition.X, 
-                                      (int)SliderPosition.Y, 200, 20);
-
-        // Установка начальной позиции ручки ползунка на основе MediaPlayer.Volume
-        int knobX = (int)(SliderPosition.X + (Volume * (SliderBarRect.Width - SliderButtonTexture.Width)));
-        // Центрирование ручки по высоте полосы
-        SliderButtonRect = new Rectangle(knobX, (int)SliderPosition.Y - 10, 20, 40);
-
-        // Установка положения кнопки mute
-        // Положение и размер кнопки
-        MuteButtonRect = new Rectangle(25, 480, MuteTexture.Width, MuteTexture.Height);
     }
 }
--- Views/SliderVolumeDrawer.cs
+++ Views/SliderVolumeDrawer.cs
@@ -7,22 +7,55 @@ namespace BloodyPath.Views;
 public class SliderVolumeDrawer
 {
     private readonly SliderVolume SliderVolume;
+    private readonly Texture2D SliderBarTexture;
+    private readonly Texture2D SliderButtonTexture;
+    private readonly Texture2D MuteTexture;
+    private readonly Texture2D UnmuteTexture;
 
-    public SliderVolumeDrawer(SliderVolume sliderVolume)
+    public SliderVolumeDrawer(SliderVolume sliderVolume,
+                              Texture2D sliderBarTexture,
+                              Texture2D sliderButtonTexture,
+                              Texture2D muteTexture,
+                              Texture2D unmuteTexture)
     {
         SliderVolume = sliderVolume;
+        SliderBarTexture = sliderBarTexture;
+        SliderButtonTexture = sliderButtonTexture;
+        MuteTexture = muteTexture;
+        UnmuteTexture = unmuteTexture;
+        LoadContent();
+    }
+
+    public void LoadContent()
+    {
+        // Установка начальной позиции
+        SliderVolume.SliderPosition = new Vector2(100, 488);
+
+        // Определение размеров и положения ползунка
+        SliderVolume.SliderBarRect = new Rectangle((int)SliderVolume.SliderPosition.X,
+                                                   (int)SliderVolume.SliderPosition.Y, 200, 20);
+
+        // Установка начальной позиции ручки ползунка на основе MediaPlayer.Volume
+        int knobX = (int)(SliderVolume.SliderPosition.X + (SliderVolume.Volume * 
+                         (SliderVolume.SliderBarRect.Width - SliderButtonTexture.Width)));
+        // Центрирование ручки по высоте полосы
+        SliderVolume.SliderButtonRect = new Rectangle(knobX, (int)SliderVolume.SliderPosition.Y - 10, 20, 40);
+
+        // Установка положения кнопки mute
+        // Положение и размер кнопки
+        SliderVolume.MuteButtonRect = new Rectangle(25, 480, MuteTexture.Width, MuteTexture.Height);
     }
 
     public void Draw(SpriteBatch spriteBatch)
     {
         // Отрисовка полосы и ручки ползунка
-        spriteBatch.Draw(SliderVolume.SliderBarTexture, SliderVolume.SliderBarRect, Color.White);
-        spriteBatch.Draw(SliderVolume.SliderButtonTexture, SliderVolume.SliderButtonRect, Color.White);
+        spriteBatch.Draw(SliderBarTexture, SliderVolume.SliderBarRect, Color.White);
+        spriteBatch.Draw(SliderButtonTexture, SliderVolume.SliderButtonRect, Color.White);
 
         // Отрисовка кнопки mute/unmute
         if (SliderVolume.IsMuted)
-            spriteBatch.Draw(SliderVolume.MuteTexture, SliderVolume.MuteButtonRect, Color.White);
+            spriteBatch.Draw(MuteTexture, SliderVolume.MuteButtonRect, Color.White);
         else
-            spriteBatch.Draw(SliderVolume.UnmuteTexture, SliderVolume.MuteButtonRect, Color.White);
+            spriteBatch.Draw(UnmuteTexture, SliderVolume.MuteButtonRect, Color.White);
     }
 }
